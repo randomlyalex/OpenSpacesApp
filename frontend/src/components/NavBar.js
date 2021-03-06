b1567fe68e0e0cb4d540c7c025123bd494bc5c12
jamiemalcolm <jmalcolm281@gmail.com>
2021-03-06 18:28:43 +0000

    created

---
+++ frontend/src/components/NavBar.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import {Link} from 'react-router-dom'
+
+const NavBar = () => {
+  return (
+    <div>
+      <Link to="/">Home</Link>
+      <p>this is our nav Bar</p>
+    </div>
+  )
+}
+
+export default NavBar
