1d8f05fea4a96eaad1d851adbeed225888a305f5
jamiemalcolm <jmalcolm281@gmail.com>
2021-03-06 18:25:00 +0000

    created main Container  with router and switch statement

---
+++ frontend/src/containers/MainContainer.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
+import NavBar from '../components/NavBar'
+import AdminContainer from './AdminContainer';
+import HomeContainer from './HomeContainer'
+
+const MainContainer = () => {
+
+  
+  return (
+    <Router>
+      <>
+
+       <NavBar/>
+       <Switch>
+        <Route exact path="/" render={() => <HomeContainer/>}/>
+        <Route path="/admin" render={() => <AdminContainer/>}/>
+       </Switch>
+      </>
+    </Router>
+  )
+}
+
+export default MainContainer
