9b2efd1f28531e923000a25fe190bc233cf24558
jamiemalcolm <jmalcolm281@gmail.com>
2021-03-06 18:29:10 +0000

    created admin container home container

---
+++ frontend/src/containers/AdminContainer.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const AdminContainer = () => {
+  return (
+    <div>
+      <p>this is the Admin Page</p>
+    </div>
+  )
+}
+
+export default AdminContainer
---
+++ frontend/src/containers/HomeContainer.js
@@ -0,0 +1,27 @@
+import React, { useEffect, useState } from 'react'
+import Request from '../helpers/request'
+
+const HomeContainer = () => {
+  
+  const [pois, setPois] = useState([]);
+  
+  const getPois = () => {
+    const request = new Request();
+    request.get("/api/pois").then((data) => {setPois(data);})
+  }
+
+  useEffect(()=> {
+    getPois()
+  }, [])
+
+  console.log(pois);
+  return (
+    <div>
+
+      <p>This is the home page</p>
+      
+    </div>
+  )
+}
+
+export default HomeContainer
