e6d4f61f8668694d642d3ae0cfe7a09c5a1bf90c
Alex Dodd <alexdodd@gmail.com>
2021-03-07 19:45:17 +0000

    Moved the Router to a higher level To App.js, the react-router-dom docs suggest to do this

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,14 +1,15 @@
 import React from 'react'
 import './App.css'
 import MainContainer from './containers/MainContainer'
+import { BrowserRouter as Router } from 'react-router-dom'
 
 function App() {
     return (
+        <Router>
         <>
-        
-        <MainContainer/>
-        
+            <MainContainer/>
         </>
+        </Router>
     )
 }
 
--- frontend/src/containers/MainContainer.js
+++ frontend/src/containers/MainContainer.js
@@ -3,18 +3,20 @@ import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 import NavBar from '../components/NavBar'
 import AdminContainer from './AdminContainer'
 import HomeContainer from './HomeContainer'
+import Button from '@material-ui/core/Button'
 
 const MainContainer = () => {
     return (
-        <Router>
             <>
                 <NavBar />
+                <Button variant="text" color="default">
+                  Test Button
+                </Button>
                 <Switch>
                     <Route exact path="/" render={() => <HomeContainer />} />
                     <Route path="/admin" render={() => <AdminContainer />} />
                 </Switch>
             </>
-        </Router>
     )
 }
 
