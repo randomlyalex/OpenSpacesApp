e1369764d42c89adb9d9e415b806376fd0954036
Alex Dodd <alexdodd@gmail.com>
2021-03-08 10:37:04 +0000

    added the findAllByType derived query

--- backend/src/main/java/com/codeclan/openspaces/repositories/PoiRepository.java
+++ backend/src/main/java/com/codeclan/openspaces/repositories/PoiRepository.java
@@ -1,9 +1,15 @@
 package com.codeclan.openspaces.repositories;
 
 import com.codeclan.openspaces.models.Poi;
+import com.codeclan.openspaces.models.Table;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface PoiRepository extends MongoRepository<Poi, String> {
+    public List<Poi> findAllByType(String type);
+
 }
