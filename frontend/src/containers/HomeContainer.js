52c826a296b6ab4f1a2813fbacacdbf4653e4789
Alex Dodd <alexdodd@gmail.com>
2021-03-08 11:23:53 +0000

    Added initial drop down fetch selection

--- frontend/src/containers/HomeContainer.js
+++ frontend/src/containers/HomeContainer.js
@@ -3,21 +3,37 @@ import Request from '../helpers/request'
 
 const HomeContainer = () => {
     const [pois, setPois] = useState([])
+    const [type, setType] = useState('')
 
     const getPois = () => {
         const request = new Request()
-        request.get('/api/pois').then((data) => {
+        request.get(`/api/pois${type}`).then((data) => {
             setPois(data)
         })
     }
 
+    const handleDropdown = (event) => {
+        if (event.target.value != '') {
+            setType(`?type=${event.target.value}`)
+        } else {
+            setType('')
+        }
+    }
+
     useEffect(() => {
         getPois()
-    }, [])
+    }, [type])
 
     return (
         <div>
             <p>This is the home page</p>
+            <select onChange={handleDropdown}>
+                <option value="">All</option>
+                <option value="bench">Bench</option>
+                <option value="table">Table</option>
+                <option value="toilet">Toilet</option>
+                <option value="space">Space</option>
+            </select>
         </div>
     )
 }
