1219d09325057020deb7525cff0f57cb13b06324
Alex Dodd <alexdodd@gmail.com>
2021-03-08 01:51:44 +0000

    removed unused imports

--- frontend/src/containers/MainContainer.js
+++ frontend/src/containers/MainContainer.js
@@ -1,19 +1,18 @@
 import React from 'react'
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+import { Route, Switch } from 'react-router-dom'
 import NavBar from '../components/NavBar'
 import AdminContainer from './AdminContainer'
 import HomeContainer from './HomeContainer'
-import Button from '@material-ui/core/Button'
 
 const MainContainer = () => {
     return (
-            <>
-                <NavBar />
-                <Switch>
-                    <Route exact path="/" render={() => <HomeContainer />} />
-                    <Route path="/admin" render={() => <AdminContainer />} />
-                </Switch>
-            </>
+        <>
+            <NavBar />
+            <Switch>
+                <Route exact path="/" render={() => <HomeContainer />} />
+                <Route path="/admin" render={() => <AdminContainer />} />
+            </Switch>
+        </>
     )
 }
 
