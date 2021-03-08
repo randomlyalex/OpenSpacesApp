de5a6051d5c3fc0ed55d47241adb5f703bf051f9
Alex Dodd <alexdodd@gmail.com>
2021-03-08 12:36:33 +0000

    Updated to use PoiSubmitForm component

--- frontend/src/containers/AdminContainer.js
+++ frontend/src/containers/AdminContainer.js
@@ -1,9 +1,22 @@
-import React from 'react'
+import React, { useRef } from 'react'
+import PoiSubmitForm from '../components/PoiSubmitForm'
+import Request from '../helpers/request'
 
 const AdminContainer = () => {
+    const addPoi = (submittedPoi) => {
+        const request = new Request()
+        request.post(`/api/${submittedPoi.type}`, submittedPoi)
+        console.log(submittedPoi)
+    }
+
     return (
         <div>
             <p>this is the Admin Page</p>
+            <PoiSubmitForm
+                onPoiSubmit={(poi) => {
+                    addPoi(poi)
+                }}
+            />
         </div>
     )
 }
