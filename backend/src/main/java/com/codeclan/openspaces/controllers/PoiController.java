8c1deb5a15366c1593b99a8bc12f7982aa9408ea
Alex Dodd <alexdodd@gmail.com>
2021-03-08 10:36:04 +0000

    tidied up get routes, and added individual post routes for models

--- backend/src/main/java/com/codeclan/openspaces/controllers/PoiController.java
+++ backend/src/main/java/com/codeclan/openspaces/controllers/PoiController.java
@@ -1,18 +1,11 @@
 package com.codeclan.openspaces.controllers;
 
-import com.codeclan.openspaces.models.Bench;
-import com.codeclan.openspaces.models.Poi;
-import com.codeclan.openspaces.models.Table;
-import com.codeclan.openspaces.repositories.BenchRepository;
-import com.codeclan.openspaces.repositories.PoiRepository;
-import com.codeclan.openspaces.repositories.TableRepository;
+import com.codeclan.openspaces.models.*;
+import com.codeclan.openspaces.repositories.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -28,21 +21,44 @@ public class PoiController {
     @Autowired
     TableRepository tableRepository;
 
+    @Autowired
+    ToiletRepository toiletRepository;
+
+    @Autowired
+    SpaceRepository spaceRepository;
+
     @GetMapping(value = "/pois")
-    public ResponseEntity<List<Poi>> getAllPois(){
+    public ResponseEntity<List<Poi>> getAllPois(
+            @RequestParam(name = "type", required = false) String type
+    ){
+        if (type != null ){ return new ResponseEntity<>(poiRepository.findAllByType(type), HttpStatus.OK);}
+
         return new ResponseEntity<>(poiRepository.findAll(), HttpStatus.OK);
     }
 
-    @PostMapping(value = "/benches")
-    public ResponseEntity<Poi> postPoi(@RequestBody Bench bench){
+       @PostMapping(value = "/benches")
+    public ResponseEntity<Poi> postBench(@RequestBody Bench bench){
         benchRepository.save(bench);
         return new ResponseEntity<>(bench, HttpStatus.CREATED);
     }
 
+
     @PostMapping(value = "/tables")
-    public ResponseEntity<Poi> postPoi(@RequestBody Table table){
+    public ResponseEntity<Poi> postTable(@RequestBody Table table){
         tableRepository.save(table);
         return new ResponseEntity<>(table, HttpStatus.CREATED);
+    }
+
+    @PostMapping(value = "/toilets")
+    public ResponseEntity<Poi> postToilet(@RequestBody Toilet toilet){
+        toiletRepository.save(toilet);
+        return new ResponseEntity<>(toilet, HttpStatus.CREATED);
+    }
+
+    @PostMapping(value = "/spaces")
+    public ResponseEntity<Poi> postSpace(@RequestBody Space space){
+        spaceRepository.save(space);
+        return new ResponseEntity<>(space, HttpStatus.CREATED);
     }
 
 }
