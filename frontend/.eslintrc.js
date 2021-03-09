15415eb00a839b56dc44118d8213eece6ef2cda7
Alex Dodd <alexdodd@gmail.com>
2021-03-09 00:07:09 +0000

    added a line to warn about prop types by defaul this errors

--- frontend/.eslintrc.js
+++ frontend/.eslintrc.js
@@ -16,6 +16,7 @@ module.exports = {
     rules: {
         'react/jsx-uses-react': 'warn',
         'react/jsx-uses-vars': 'warn',
+        'react/prop-types': ['warn'],
         'no-restricted-imports': [
             'warn',
             {
