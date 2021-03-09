f2ba9db526a0365ddb4db894b580a7b2a3b7ddf9
Alex Dodd <alexdodd@gmail.com>
2021-03-09 00:10:02 +0000

    added a button to locate me used the setCentreMap, it didnt work, but i think it just needs playing it also didnt break anything! removed the top diable line too why cant we map outside the jsx?

--- frontend/src/components/LeafletMap.js
+++ frontend/src/components/LeafletMap.js
@@ -1,39 +1,53 @@
-/* eslint react/prop-types: 0 */
 import React, { useState } from 'react'
 import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet'
-import "../App.css"
+import '../App.css'
+import PropTypes from 'prop-types'
+import Button from '@material-ui/core/Button'
+import Typography from '@material-ui/core/Typography'
+const LeafletMap = ({ pois }) => {
+    const [centreMap, setCentreMap] = useState([55.9533, -3.1883])
 
-const LeafletMap = ({pois}) => {
+    const handleLocateMe = () => {
+        setCentreMap([55.9533, -3.1883])
+    }
 
-  const [centerMap, setCenterMap] = useState('')
-
-
-  return (
-    <>
-
-      <MapContainer center={[55.9533, -3.1883]} zoom={11} scrollWheelZoom={true}>
-        <TileLayer
-          attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
-          <Marker position={[55.971, -3.165934]}>
-    <Popup>
-      example of our first poi in the DB <br /> Easily or not so easily customizable.
-    </Popup>
-  </Marker>
-        {pois.map((poi) => {
-          return <Marker
-          key={poi.id} 
-          position={[poi.coord.lat, poi.coord.lon]}>
-            <Popup>
-              <h2>{poi.type}</h2>
-            </Popup>
-          </Marker>
-        })}
-      </MapContainer>
-
-    </>
-  )
+    return (
+        <>
+            <Button
+                variant="contained"
+                color="secondary"
+                onClick={handleLocateMe}
+            >
+                <Typography color="initial">Locate Me</Typography>
+            </Button>
+            <MapContainer center={centreMap} zoom={11} scrollWheelZoom={true}>
+                <TileLayer
+                    attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+                    url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                />
+                {
+                    // why can't this is a map function up top?
+                    pois.map((poi) => {
+                        return (
+                            <Marker
+                                key={poi.id}
+                                position={[poi.coord.lat, poi.coord.lon]}
+                            >
+                                <Popup>
+                                    <h2>{poi.type}</h2>
+                                </Popup>
+                            </Marker>
+                        )
+                    })
+                }
+            </MapContainer>
+        </>
+    )
 }
-
+// see: https://reactjs.org/docs/typechecking-with-proptypes.html
+// & https://www.npmjs.com/package/prop-types
+LeafletMap.propTypes = {
+    pois: PropTypes.array.isRequired,
+}
 
 export default LeafletMap
