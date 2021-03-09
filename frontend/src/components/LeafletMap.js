c969b3db9da13feab0ffe411d089be7f32bb1557
jamiemalcolm <jmalcolm281@gmail.com>
2021-03-09 15:27:31 +0000

    can get user location
    from one click on map

--- frontend/src/components/LeafletMap.js
+++ frontend/src/components/LeafletMap.js
@@ -1,30 +1,59 @@
 import React, { useState } from 'react'
-import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet'
+import { MapContainer, TileLayer, Marker, Popup, useMapEvents } from 'react-leaflet'
 import '../App.css'
 import PropTypes from 'prop-types'
 import Button from '@material-ui/core/Button'
 import Typography from '@material-ui/core/Typography'
+
+
+function LocationMarker() {
+  const [position, setPosition] = useState(null)
+  const map = useMapEvents({
+    click() {
+      map.locate()
+  
+      
+    },
+    locationfound(e) {
+      setPosition(e.latlng)
+      map.flyTo(e.latlng, map.getZoom())
+    },
+  })
+
+  return position === null ? null : (
+    <Marker position={position}>
+      <Popup>You are here</Popup>
+    </Marker>
+  )
+  
+}
+
+
 const LeafletMap = ({ pois }) => {
     const [centreMap, setCentreMap] = useState([55.9533, -3.1883])
-
-    const handleLocateMe = () => {
-        setCentreMap([55.9533, -3.1883])
-    }
-
+    
     return (
         <>
             <Button
                 variant="contained"
                 color="secondary"
-                onClick={handleLocateMe}
+                
             >
                 <Typography color="initial">Locate Me</Typography>
             </Button>
-            <MapContainer center={centreMap} zoom={11} scrollWheelZoom={true}>
+            <MapContainer center={centreMap} 
+            zoom={15} 
+            scrollWheelZoom={true}
+            
+            >
+              
+                <Typography color="initial">Locate Me</Typography>
+          
                 <TileLayer
                     attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
                     url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
                 />
+               
                 {
                     // why can't this is a map function up top?
                     pois.map((poi) => {
@@ -40,6 +69,8 @@ const LeafletMap = ({ pois }) => {
                         )
                     })
                 }
+               
+                <LocationMarker/>
             </MapContainer>
         </>
     )
