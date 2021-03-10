f32eb4a15b29d42575313236ab0f160ccd2136a6
Alex Dodd <alexdodd@gmail.com>
2021-03-10 23:05:41 +0000

    testing admin side container

---
+++ frontend/src/containers/UserAdminContainer.js
@@ -0,0 +1,21 @@
+import React from 'react'
+
+const UserAdminContainer = () => {
+    return (
+        <>
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
+            User admin
+        </>
+    )
+}
+
+export default UserAdminContainer
