12fd630e95f1113c42569bc899483e355f2987a2
jamiemalcolm <jmalcolm281@gmail.com>
2021-03-10 15:13:44 +0000

    created logout button component

---
+++ frontend/src/components/LogOutButton.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import {useAuth0} from '@auth0/auth0-react';
+
+
+const LogOutButton = () => {
+  const {logout, isAuthenticated } = useAuth0();
+
+  return (
+
+    isAuthenticated &&(
+      <button
+      onClick={() => logout()}>
+      Logout
+    </button>
+      )
+
+  )
+}
+
+export default LogOutButton
