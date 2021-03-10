9cada090a2bc09897c039675b4a751ccfa708735
Alex Dodd <alexdodd@gmail.com>
2021-03-10 00:30:00 +0000

    full MUI Skin, added radio buttons had to refactor how we calle the api with null

--- frontend/src/containers/HomeContainer.js
+++ frontend/src/containers/HomeContainer.js
@@ -1,53 +1,108 @@
+import {
+    Checkbox,
+    Container,
+    CssBaseline,
+    FormControl,
+    FormControlLabel,
+    FormLabel,
+    Grid,
+    Radio,
+    Select,
+    RadioGroup,
+} from '@material-ui/core'
 import React, { useEffect, useState } from 'react'
 import LeafletMap from '../components/LeafletMap'
 import Request from '../helpers/request'
 
 const HomeContainer = () => {
     const [pois, setPois] = useState([])
-    const [type, setType] = useState('')
+    const [type, setType] = useState('all')
 
     const getPois = () => {
         const request = new Request()
-        request.get(`/api/pois${type}`).then((data) => {
+        request.get(`/api/pois?type=${type}`).then((data) => {
             setPois(data)
         })
     }
 
-    const PoisList = pois.map((poi) => {
-        return (
-            <h3 key={poi.id}>
-                {poi.id} - {poi.type}
-                {poi.lat} - {poi.lon}
-            </h3>
-        )
-    })
-
-    const handleDropdown = (event) => {
-        if (event.target.value != '') {
-            setType(`?type=${event.target.value}`)
-        } else {
-            setType('')
-        }
-    }
+    // const PoisList = pois.map((poi) => {
+    //     return (
+    //         <h3 key={poi.id}>
+    //             {poi.id} - {poi.type}
+    //             {poi.lat} - {poi.lon}
+    //         </h3>
+    //     )
+    // })
 
     useEffect(() => {
         getPois()
     }, [type])
 
-    return (
-        <div>
-            <br />
-            <label>Filter Points of Interest by Type:</label>
-            <select onChange={handleDropdown}>
-                <option value="">All</option>
-                <option value="bench">Bench</option>
-                <option value="table">Table</option>
-                <option value="toilet">Toilet</option>
-                <option value="space">Space</option>
-            </select>
+    useEffect(() => {
+        getPois()
+    }, [])
 
-            <LeafletMap pois={pois} />
-        </div>
+    const handleRadio = (event) => {
+        setType(event.target.value)
+    }
+
+    return (
+        <>
+            <Container component="main" justify="center" alignContent="center">
+                <CssBaseline />
+                <Grid
+                    container
+                    spacing={0}
+                    justify="center"
+                    alignContent="center"
+                >
+                    <Grid item xs={12} justify="center" alignContent="center">
+                        <FormControl component="fieldset">
+                            <FormLabel component="legend">
+                                Filter by Type
+                            </FormLabel>
+                            <RadioGroup
+                                row
+                                aria-label="gender"
+                                name="type"
+                                value={type}
+                                onChange={handleRadio}
+                            >
+                                <FormControlLabel
+                                    value="all"
+                                    control={<Radio />}
+                                    label="All"
+                                />
+                                <FormControlLabel
+                                    value="bench"
+                                    control={<Radio />}
+                                    label="Bench"
+                                />
+                                <FormControlLabel
+                                    value="table"
+                                    control={<Radio />}
+                                    label="Table"
+                                />
+                                <FormControlLabel
+                                    value="toilet"
+                                    control={<Radio />}
+                                    label="Toilet"
+                                />
+                                <FormControlLabel
+                                    value="space"
+                                    control={<Radio />}
+                                    label="Space"
+                                />
+                            </RadioGroup>
+                        </FormControl>
+                    </Grid>
+
+                    <Grid item xs={12} justify="center" alignContent="center">
+                        <LeafletMap pois={pois} />
+                    </Grid>
+                </Grid>
+            </Container>
+        </>
     )
 }
 
