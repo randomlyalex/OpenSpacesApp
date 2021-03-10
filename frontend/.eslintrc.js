217e89c0c07a205d6fff43d84c67c68cc2655285
jamiemalcolm <jmalcolm281@gmail.com>
2021-03-10 15:11:00 +0000

    added linting rules
    for process.env
    and added .env to git ignore

--- frontend/.eslintrc.js
+++ frontend/.eslintrc.js
@@ -3,6 +3,8 @@ module.exports = {
     env: {
         browser: true,
         es2021: true,
+        node: true,
+        commonjs: true
     },
     extends: ['eslint:recommended', 'plugin:react/recommended'],
     parserOptions: {
