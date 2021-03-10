890dbd41a7ec132946506b41a899a3af9826d8ff
Alex Dodd <alexdodd@gmail.com>
2021-03-10 00:27:44 +0000

    fixed the map css to make it more responsive

--- frontend/src/components/LeafletMap.js
+++ frontend/src/components/LeafletMap.js
@@ -1,43 +1,47 @@
 import React, { useState } from 'react'
-import { MapContainer, TileLayer, Marker, Popup, useMapEvents } from 'react-leaflet'
+import {
+    MapContainer,
+    TileLayer,
+    Marker,
+    Popup,
+    useMapEvents,
+} from 'react-leaflet'
 import '../App.css'
 import PropTypes from 'prop-types'
 import Typography from '@material-ui/core/Typography'
 
-
 function LocationMarker() {
-  const [position, setPosition] = useState(null)
-  const map = useMapEvents({
-    dblclick() {
-      map.locate()
-  
-      
-    },
-    locationfound(e) {
-      setPosition(e.latlng)
-      map.flyTo(e.latlng, map.getZoom())
-    },
-  })
+    const [position, setPosition] = useState(null)
+    const map = useMapEvents({
+        dblclick() {
+            map.locate()
+        },
+        locationfound(e) {
+            setPosition(e.latlng)
+            map.flyTo(e.latlng, map.getZoom())
+        },
+    })
 
-  return position === null ? null : (
-    <Marker position={position}>
-      <Popup>You are here</Popup>
-    </Marker>
-  )
-  
+    return position === null ? null : (
+        <Marker position={position}>
+            <Popup>You are here</Popup>
+        </Marker>
+    )
 }
-
 
 const LeafletMap = ({ pois }) => {
     const [centreMap, setCentreMap] = useState([55.9533, -3.1883])
-    
+
     return (
         <>
-        <h3>Double Click on the map to find your location</h3>
-            <MapContainer center={centreMap} 
-            zoom={15} 
-            scrollWheelZoom={true}
-            doubleClickZoom={false}
+            <Typography variant="sub">
+                Double Click on the map to find your location
+            </Typography>
+            <MapContainer
+                center={centreMap}
+                zoom={15}
+                scrollWheelZoom={true}
+                doubleClickZoom={false}
             >
                 <TileLayer
                     attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
@@ -53,13 +57,16 @@ const LeafletMap = ({ pois }) => {
                                     <p>Space For: {poi.capacity}</p>
                                     <p>Accessibility: {poi.accessibility}</p>
                                     <p>Privacy: {poi.privacy}</p>
-                                    <p>Is Sheltered? :{poi.sheltered.toString()}</p>
+                                    <p>
+                                        Is Sheltered? :
+                                        {poi.sheltered.toString()}
+                                    </p>
                                 </Popup>
                             </Marker>
                         )
                     })
                 }
-                <LocationMarker/>
+                <LocationMarker />
             </MapContainer>
         </>
     )
