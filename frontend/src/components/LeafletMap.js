38c8a9c338148662549eae663d613b3fbdebd6c4
jamiemalcolm <jmalcolm281@gmail.com>
2021-03-10 19:12:08 +0000

    add to map
    trying to do a call back to get the lat lng
    
    getting an error about uncotrolled properties trying to be controlled

--- frontend/src/components/LeafletMap.js
+++ frontend/src/components/LeafletMap.js
@@ -12,6 +12,7 @@ import PropTypes from 'prop-types'
 import Typography from '@material-ui/core/Typography'
 import { SettingsInputAntennaTwoTone } from '@material-ui/icons'
 import PoiSubmitForm from './PoiSubmitForm'
+import Request from '../helpers/request'
 
 function UserLocationMarker() {
     const [position, setPosition] = useState(null)
@@ -29,38 +30,64 @@ function UserLocationMarker() {
     )
 }
 
-function AddMarker() {
+function AddMarker({getLl}) {
+
+    
     const [marker, setMarker] = useState(null)
     const map = useMapEvents({
         click(e) {
             const newMarker = e.latlng
             setMarker(newMarker)
-            console.log(newMarker)
         },
     })
+    if(marker != null){
+        getLl(marker.lat, marker.lng)
+    }else{
+        console.log("waiting for marker click")
+    }
     return marker === null ? null : (
         <Marker position={marker}>
-            <Popup>You are here</Popup>
+            <Popup>{marker.lat}, {marker.lng}</Popup>
         </Marker>
     )
 }
-
-
 //toggle add for = flase function to set toggleaddForm = !toggleAddForm 
-
-
-
-
-
-
-
-
-
 const LeafletMap = ({ pois }) => {
 
-
     const [centreMap, setCentreMap] = useState([55.9533, -3.1883])
-  
+
+    const [latForForm, setLatForForm] = useState();
+    const [lonForForm, setlonForForm] = useState();
+
+    const getLatLngForForm = (lat, lng) => {
+        setLatForForm(lat)
+        setlonForForm(lng)
+    }
+    
+    const serverUrl = process.env.REACT_APP_API_SERVER
+    const addPoi = (submittedPoi) => {
+        const request = new Request()
+        let endPoint = null
+        switch (submittedPoi.type) {
+            case 'bench':
+                endPoint = 'benches'
+                break
+            case 'table':
+                endPoint = 'tables'
+                break
+            case 'toilet':
+                endPoint = 'toilets'
+                break
+            case 'space':
+                endPoint = 'spaces'
+                break
+        }
+        if (endPoint != null) {
+            request.post(`${serverUrl}/api/${endPoint}`, submittedPoi)
+        }
+        console.log(endPoint)
+        console.log(submittedPoi)
+    }
 
     return (
         <>
@@ -68,7 +95,12 @@ const LeafletMap = ({ pois }) => {
                 Double Click on the map to find your location
             </Typography>
 
-            <PoiSubmitForm/>
+            <PoiSubmitForm
+            onPoiSubmit={(poi) => {
+                addPoi(poi)
+            }}
+            lat={latForForm}
+            lon={lonForForm}/>
 
             <MapContainer
                 center={centreMap}
@@ -112,7 +144,7 @@ const LeafletMap = ({ pois }) => {
                     })
                 }
                 <UserLocationMarker />
-                <AddMarker/>
+                <AddMarker getLl={getLatLngForForm}/>
                
             </MapContainer>
 
