c2142c79c7d7d3af3af9260a3a5acc9f7f1557e8
Alex Dodd <alexdodd@gmail.com>
2021-03-10 00:29:09 +0000

    Full reskin of the POI submit form Full MUI treatment feeds into react-hooks-form submits to the same hook and into mongo

--- frontend/src/components/PoiSubmitForm.js
+++ frontend/src/components/PoiSubmitForm.js
@@ -1,75 +1,238 @@
 /* eslint react/prop-types: 0 */
-import React, { useState } from 'react'
-import { useForm } from 'react-hook-form'
+
+import {
+    Container,
+    CssBaseline,
+    Avatar,
+    Typography,
+    Grid,
+    Button,
+    TextField,
+    Checkbox,
+    FormControlLabel,
+    makeStyles,
+    Input,
+    Select,
+    MenuItem,
+    InputLabel,
+    FormControl,
+} from '@material-ui/core'
+import { AddLocation } from '@material-ui/icons'
+import React from 'react'
+import { useForm, Controller } from 'react-hook-form'
 import '../App.css'
 
 const PoiSubmitForm = ({ onPoiSubmit, lat, lon }) => {
-    const { handleSubmit, register } = useForm({})
+    const { handleSubmit, register, control } = useForm()
 
-    const onSubmit = (formData) => {
+    const onSubmit = (formData, event) => {
         onPoiSubmit(formData)
+        event.target.reset()
     }
 
-    return (
-        <div>
-            <h4> New Form Test</h4>
-            <form onSubmit={handleSubmit(onSubmit)}>
-                <label>PoiType</label>
-                <select name="type" ref={register}>
-                    <option value="bench">Bench</option>
-                    <option value="table">Table</option>
-                    <option value="toilet">Toilet</option>
-                    <option value="space">Space</option>
-                </select>
-                <br />
-                <label>Location</label>
-                <br />
-                <span>
-                    <input
-                        type="text"
-                        name="lat"
-                        placeholder="lat?"
-                        ref={register}
-                    />
+    const useStyles = makeStyles((theme) => ({
+        paper: {
+            marginTop: theme.spacing(8),
+            display: 'flex',
+            flexDirection: 'column',
+            alignItems: 'center',
+        },
+        avatar: {
+            margin: theme.spacing(1),
+            backgroundColor: theme.palette.secondary.main,
+        },
+        form: {
+            width: '100%', // Fix IE 11 issue.
+            marginTop: theme.spacing(3),
+        },
+        submit: {
+            margin: theme.spacing(3, 0, 2),
+        },
+    }))
 
-                    <input
-                        type="text"
-                        name="lon"
-                        placeholder="lon?"
-                        ref={register}
-                    />
-                </span>
-                <br />
-                <label>Privacy</label>
-                <br />
-                <input type="text" name="privacy" ref={register} />
-                <br />
-                <label>Accessibility</label>
-                <br />
-                <input type="text" name="accessibility" ref={register} />
-                <br />
-                <label>Capacity</label>
-                <br />
-                <input type="number" name="capacity" ref={register} />
-                <br />
-                <span>
-                    <label>Sheltered: </label>
-                    <div ref={register}>
-                        Yes
-                        <input type="radio" name="sheltered" value="true" />
-                        No
+    const classes = useStyles()
+
+    return (
+        <>
+            <Container component="main" maxWidth="xs">
+                <CssBaseline />
+                <div className={classes.paper}>
+                    <Avatar className={classes.avatar}>
+                        <AddLocation />
+                    </Avatar>
+                    <Typography component="h1" variant="h5">
+                        Add Poi
+                    </Typography>
+                    <form
+                        className={classes.form}
+                        noValidate
+                        onSubmit={handleSubmit(onSubmit)}
+                    >
+                        <Grid container spacing={2}>
+                            <Grid item xs={12}>
+                                <Controller
+                                    name="type"
+                                    control={control}
+                                    as={Select}
+                                    required
+                                    label="Poi Type"
+                                >
+                                    <MenuItem value="" disabled>
+                                        Poi Type
+                                    </MenuItem>
+                                    <MenuItem value={'bench'}>Bench</MenuItem>
+                                    <MenuItem value={'table'}>Table</MenuItem>
+                                    <MenuItem value={'toilet'}>Toilet</MenuItem>
+                                    <MenuItem value={'space'}>Space</MenuItem>
+                                </Controller>
+                            </Grid>
+                            <Grid item xs={12} sm={6}>
+                                <TextField
+                                    name="lat"
+                                    variant="outlined"
+                                    defaultValue={lat}
+                                    required
+                                    inputRef={register}
+                                    fullWidth
+                                    id="lat"
+                                    label="Lat"
+                                    autoFocus
+                                    type="number"
+                                />
+                            </Grid>
+                            <Grid item xs={12} sm={6}>
+                                <TextField
+                                    variant="outlined"
+                                    defaultValue={lon}
+                                    required
+                                    inputRef={register}
+                                    fullWidth
+                                    id="lon"
+                                    label="Lon"
+                                    name="lon"
+                                    type="number"
+                                />
+                            </Grid>
+                            <Grid item xs={12}>
+                                <TextField
+                                    variant="outlined"
+                                    required
+                                    inputRef={register}
+                                    fullWidth
+                                    id="privacy"
+                                    label="Privacy"
+                                    name="privacy"
+                                />
+                            </Grid>
+                            <Grid item xs={12}>
+                                <TextField
+                                    variant="outlined"
+                                    required
+                                    inputRef={register}
+                                    fullWidth
+                                    id="accessibility"
+                                    label="Accessibility"
+                                    name="accessibility"
+                                />
+                            </Grid>
+                            <Grid item xs={12} sm={6}>
+                                <TextField
+                                    variant="outlined"
+                                    fullWidth
+                                    type="number"
+                                    name="capacity"
+                                    label="Capacity"
+                                    id="capacity"
+                                />
+                            </Grid>
+                            <Grid item xs={12} sm={6}>
+                                <FormControlLabel
+                                    labelPlacement="start"
+                                    control={
+                                        <Controller
+                                            as={Checkbox}
+                                            control={control}
+                                            name="sheltered"
+                                            color="primary"
+                                        />
+                                    }
+                                    label="Sheltered"
+                                />
+                            </Grid>
+                        </Grid>
+                        <Button
+                            type="submit"
+                            fullWidth
+                            variant="contained"
+                            color="primary"
+                            className={classes.submit}
+                        >
+                            Add!
+                        </Button>
+                    </form>
+                </div>
+            </Container>
+            {/* 
+            <div>
+                <h4> New Form Test</h4>
+                <form onSubmit={handleSubmit(onSubmit)}>
+                    <label>PoiType</label>
+                    <select name="type" ref={register}>
+                        <option value="bench">Bench</option>
+                        <option value="table">Table</option>
+                        <option value="toilet">Toilet</option>
+                        <option value="space">Space</option>
+                    </select>
+                    <br />
+                    <label>Location</label>
+                    <br />
+                    <span>
                         <input
-                            type="radio"
-                            name="sheltered"
-                            value="false"
-                            defaultChecked
+                            type="text"
+                            name="lat"
+                            placeholder="lat?"
+                            ref={register}
                         />
-                    </div>
-                </span>
-                <br />
-                <input type="submit" />
-            </form>
-        </div>
+
+                        <input
+                            type="text"
+                            name="lon"
+                            placeholder="lon?"
+                            ref={register}
+                        />
+                    </span>
+                    <br />
+                    <label>Privacy</label>
+                    <br />
+                    <input type="text" name="privacy" ref={register} />
+                    <br />
+                    <label>Accessibility</label>
+                    <br />
+                    <input type="text" name="accessibility" ref={register} />
+                    <br />
+                    <label>Capacity</label>
+                    <br />
+                    <input type="number" name="capacity" ref={register} />
+                    <br />
+                    <span>
+                        <label>Sheltered: </label>
+                        <div ref={register}>
+                            Yes
+                            <input type="radio" name="sheltered" value="true" />
+                            No
+                            <input
+                                type="radio"
+                                name="sheltered"
+                                value="false"
+                                defaultChecked
+                            />
+                        </div>
+                    </span>
+                    <br />
+                    <input type="submit" />
+                </form>
+            </div> */}
+        </>
     )
 }
 
