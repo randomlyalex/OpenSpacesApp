0b6e17515dc6aa30e98aec8e4ed92c1f084bf717
Alex Dodd <alexdodd@gmail.com>
2021-03-10 23:22:06 +0000

    added the correct container tag to the form

--- frontend/src/components/PoiSubmitForm.js
+++ frontend/src/components/PoiSubmitForm.js
@@ -54,121 +54,123 @@ const PoiSubmitForm = ({ onPoiSubmit, lat, lon }) => {
 
     return (
         <>
-            <div className={classes.paper}>
-                <Avatar className={classes.avatar}>
-                    <AddLocation />
-                </Avatar>
-                <Typography component="h1" variant="h5">
-                    Add Poi
-                </Typography>
-                <form
-                    className={classes.form}
-                    noValidate
-                    onSubmit={handleSubmit(onSubmit)}
-                >
-                    <Grid container spacing={2}>
-                        <Grid item xs={12}>
-                            <Controller
-                                name="type"
-                                control={control}
-                                as={Select}
-                                required
-                                label="Poi Type"
-                            >
-                                <MenuItem value="" disabled>
-                                    Poi Type
-                                </MenuItem>
-                                <MenuItem value={'bench'}>Bench</MenuItem>
-                                <MenuItem value={'table'}>Table</MenuItem>
-                                <MenuItem value={'toilet'}>Toilet</MenuItem>
-                                <MenuItem value={'space'}>Space</MenuItem>
-                            </Controller>
-                        </Grid>
-                        <Grid item xs={12} sm={6}>
-                            <TextField
-                                name="lat"
-                                variant="outlined"
-                                defaultValue={lat}
-                                required
-                                inputRef={register}
-                                fullWidth
-                                id="lat"
-                                label="Lat"
-                                autoFocus
-                                type="number"
-                            />
-                        </Grid>
-                        <Grid item xs={12} sm={6}>
-                            <TextField
-                                variant="outlined"
-                                defaultValue={lon}
-                                required
-                                inputRef={register}
-                                fullWidth
-                                id="lon"
-                                label="Lon"
-                                name="lon"
-                                type="number"
-                            />
-                        </Grid>
-                        <Grid item xs={12}>
-                            <TextField
-                                variant="outlined"
-                                required
-                                inputRef={register}
-                                fullWidth
-                                id="privacy"
-                                label="Privacy"
-                                name="privacy"
-                            />
-                        </Grid>
-                        <Grid item xs={12}>
-                            <TextField
-                                variant="outlined"
-                                required
-                                inputRef={register}
-                                fullWidth
-                                id="accessibility"
-                                label="Accessibility"
-                                name="accessibility"
-                            />
-                        </Grid>
-                        <Grid item xs={12} sm={6}>
-                            <TextField
-                                variant="outlined"
-                                fullWidth
-                                type="number"
-                                name="capacity"
-                                label="Capacity"
-                                id="capacity"
-                            />
-                        </Grid>
-                        <Grid item xs={12} sm={6}>
-                            <FormControlLabel
-                                labelPlacement="start"
-                                control={
-                                    <Controller
-                                        as={Checkbox}
-                                        control={control}
-                                        name="sheltered"
-                                        color="primary"
-                                    />
-                                }
-                                label="Sheltered"
-                            />
-                        </Grid>
-                    </Grid>
-                    <Button
-                        type="submit"
-                        fullWidth
-                        variant="contained"
-                        color="primary"
-                        className={classes.submit}
+            <Container maxWidth="sm">
+                <div className={classes.paper}>
+                    <Avatar className={classes.avatar}>
+                        <AddLocation />
+                    </Avatar>
+                    <Typography component="h1" variant="h5">
+                        Add Poi
+                    </Typography>
+                    <form
+                        className={classes.form}
+                        noValidate
+                        onSubmit={handleSubmit(onSubmit)}
                     >
-                        Add!
-                    </Button>
-                </form>
-            </div>
+                        <Grid container spacing={2}>
+                            <Grid item xs={12}>
+                                <Controller
+                                    name="type"
+                                    control={control}
+                                    as={Select}
+                                    required
+                                    label="Poi Type"
+                                >
+                                    <MenuItem value="" disabled>
+                                        Poi Type
+                                    </MenuItem>
+                                    <MenuItem value={'bench'}>Bench</MenuItem>
+                                    <MenuItem value={'table'}>Table</MenuItem>
+                                    <MenuItem value={'toilet'}>Toilet</MenuItem>
+                                    <MenuItem value={'space'}>Space</MenuItem>
+                                </Controller>
+                            </Grid>
+                            <Grid item xs={12} sm={6}>
+                                <TextField
+                                    name="lat"
+                                    variant="outlined"
+                                    defaultValue={lat}
+                                    required
+                                    inputRef={register}
+                                    fullWidth
+                                    id="lat"
+                                    label="Lat"
+                                    autoFocus
+                                    type="number"
+                                />
+                            </Grid>
+                            <Grid item xs={12} sm={6}>
+                                <TextField
+                                    variant="outlined"
+                                    defaultValue={lon}
+                                    required
+                                    inputRef={register}
+                                    fullWidth
+                                    id="lon"
+                                    label="Lon"
+                                    name="lon"
+                                    type="number"
+                                />
+                            </Grid>
+                            <Grid item xs={12}>
+                                <TextField
+                                    variant="outlined"
+                                    required
+                                    inputRef={register}
+                                    fullWidth
+                                    id="privacy"
+                                    label="Privacy"
+                                    name="privacy"
+                                />
+                            </Grid>
+                            <Grid item xs={12}>
+                                <TextField
+                                    variant="outlined"
+                                    required
+                                    inputRef={register}
+                                    fullWidth
+                                    id="accessibility"
+                                    label="Accessibility"
+                                    name="accessibility"
+                                />
+                            </Grid>
+                            <Grid item xs={12} sm={6}>
+                                <TextField
+                                    variant="outlined"
+                                    fullWidth
+                                    type="number"
+                                    name="capacity"
+                                    label="Capacity"
+                                    id="capacity"
+                                />
+                            </Grid>
+                            <Grid item xs={12} sm={6}>
+                                <FormControlLabel
+                                    labelPlacement="start"
+                                    control={
+                                        <Controller
+                                            as={Checkbox}
+                                            control={control}
+                                            name="sheltered"
+                                            color="primary"
+                                        />
+                                    }
+                                    label="Sheltered"
+                                />
+                            </Grid>
+                        </Grid>
+                        <Button
+                            type="submit"
+                            fullWidth
+                            variant="contained"
+                            color="primary"
+                            className={classes.submit}
+                        >
+                            Add!
+                        </Button>
+                    </form>
+                </div>
+            </Container>
         </>
     )
 }
