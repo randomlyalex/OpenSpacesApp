2aae66cdeb581bf81af72253fe487f32263bcce3
jamiemalcolm <jmalcolm281@gmail.com>
2021-03-11 07:56:31 +0000

    updated class
    to have created by taken in constructor
    and set to that value. might need changing

--- backend/src/main/java/com/codeclan/openspaces/models/Toilet.java
+++ backend/src/main/java/com/codeclan/openspaces/models/Toilet.java
@@ -4,9 +4,10 @@ import org.springframework.data.mongodb.core.mapping.Document;
 
 @Document(collection = "pois")
 public class Toilet extends Poi{
-    public Toilet(double lat, double lon, String accessibility, String privacy) {
+    public Toilet(double lat, double lon, String accessibility, String privacy, String createdBy) {
         super(lat, lon, accessibility, privacy);
         this.setType("toilet");
+        setCreatedBy(createdBy);
 
     }
 
