565509e8720775d1997fe60d9637858db5407920
jamiemalcolm <jmalcolm281@gmail.com>
2021-03-11 11:12:13 +0000

    added rated by AL and g&s

--- backend/src/main/java/com/codeclan/openspaces/models/Poi.java
+++ backend/src/main/java/com/codeclan/openspaces/models/Poi.java
@@ -16,15 +16,9 @@ public class Poi {
     private String accessibility;
     private String privacy;
     private String type;
-    private String createdBy;
+    private ArrayList<Rating> ratedBy;
 
-    public String getCreatedBy() {
-        return createdBy;
-    }
 
-    public void setCreatedBy(String createdBy) {
-        this.createdBy = createdBy;
-    }
 
     public Poi(double lat, double lon, String accessibility, String privacy) {
         this.lat = lat;
@@ -32,7 +26,23 @@ public class Poi {
         this.accessibility = accessibility;
         this.privacy = privacy;
         this.type = "poi";
-        this.createdBy = "Admin";
+        this.ratedBy = new ArrayList<>();
+    }
+//    public Poi(double lat, double lon, String accessibility, String privacy, ArrayList<Rating> ratedBy) {
+//        this.lat = lat;
+//        this.lon= lon;
+//        this.accessibility = accessibility;
+//        this.privacy = privacy;
+//        this.type = "poi";
+//        this.ratedBy = ratedBy;
+//    }
+
+    public ArrayList<Rating> getRatedBy() {
+        return ratedBy;
+    }
+
+    public void setRatedBy(ArrayList<Rating> ratedBy) {
+        this.ratedBy = ratedBy;
     }
 
     public String getType() {
