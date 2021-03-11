89043633a83b93a1c710a92b620b305fd44de638
jamiemalcolm <jmalcolm281@gmail.com>
2021-03-11 11:14:05 +0000

    added createdBy and fav AL with G&S

--- backend/src/main/java/com/codeclan/openspaces/models/Space.java
+++ backend/src/main/java/com/codeclan/openspaces/models/Space.java
@@ -2,17 +2,45 @@ package com.codeclan.openspaces.models;
 
 import org.springframework.data.mongodb.core.mapping.Document;
 
+import java.util.ArrayList;
+
 @Document(collection = "pois")
 public class Space extends Poi {
 
     private boolean sheltered;
-
+    private String createdBy;
+    private ArrayList<String> favBy;
 
     public Space(double lat, double lon, String accessibility, String privacy, boolean sheltered, String createdBy) {
         super(lat, lon, accessibility, privacy);
         this.sheltered = sheltered;
         this.setType("space");
-        setCreatedBy(createdBy);
+        this.createdBy = createdBy;
+        this.favBy = new ArrayList<>();
+    }
+
+//    public Space(double lat, double lon, String accessibility, String privacy, boolean sheltered, String createdBy, ArrayList<String> favBy, ArrayList<Rating> ratedBy) {
+//        super(lat, lon, accessibility, privacy, ratedBy);
+//        this.sheltered = sheltered;
+//        this.setType("space");
+//        this.createdBy = createdBy;
+//        this.favBy = favBy;
+//    }
+
+    public ArrayList<String> getFavBy() {
+        return favBy;
+    }
+
+    public void setFavBy(ArrayList<String> favBy) {
+        this.favBy = favBy;
+    }
+
+    public String getCreatedBy() {
+        return createdBy;
+    }
+
+    public void setCreatedBy(String createdBy) {
+        this.createdBy = createdBy;
     }
 
     public boolean isSheltered() {
