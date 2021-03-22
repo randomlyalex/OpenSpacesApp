7f6468b7e2f85ad4ddd0f3d1f585a1631613ddf7
Alex Dodd <alexdodd@gmail.com>
2021-03-22 13:53:19 +0000

    tweak density and lines per page

--- frontend/src/components/POIList.js
+++ frontend/src/components/POIList.js
@@ -259,8 +259,8 @@ export default function POIList({ pois: rows }) {
     const [orderBy, setOrderBy] = React.useState('lat')
     const [selected, setSelected] = React.useState([])
     const [page, setPage] = React.useState(0)
-    const [dense, setDense] = React.useState(false)
-    const [rowsPerPage, setRowsPerPage] = React.useState(5)
+    const [dense, setDense] = React.useState(true)
+    const [rowsPerPage, setRowsPerPage] = React.useState(15)
 
     const handleRequestSort = (event, property) => {
         const isAsc = orderBy === property && order === 'asc'
